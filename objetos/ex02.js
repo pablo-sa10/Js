d243eedafddae8e7b3672d2ce5111351d8cf0dae
Pablo Roberto <pablomoura2016@gmail.com>
2023-12-05 08:50:15 -0300

    tarefas exercicio

--- objetos/ex02.js
+++ objetos/ex02.js
@@ -1,28 +1,71 @@
 //lista de tarefas
 const tarefas = {
-    tarefa_1: [{
-            id: 1,
-            descricao: "lavar carro",
-            concluida: false,
-        }
-    ],
-    tarefa_2: [{
-            id: 2,
-            descricao: "limpar casa",
-            concluida: false,
-        }
-    ],
-    tarefa_3: [{
-            id: 3,
-            descricao: "levar cachorro para passear",
-            concluida: false,
-        }
-    ],
-    //Adicionar uma nova tarefa à lista.
+    tarefa_1: {
+        id: 1,
+        descricao: "lavar carro",
+        concluida: false,
+},
+    tarefa_2: {
+        id: 2,
+        descricao: "limpar casa",
+        concluida: false,
+},
+    tarefa_3: {
+        id: 3,
+        descricao: "levar cachorro para passear",
+        concluida: false,
+    },
 }
-//adicionarTarefa(tarefas, "estudar")
+//Criando novas tarefa à lista.
+const novaTarefa1 = {
+        id: 4,
+        descricao: "estudar Js",
+        concluida: false
+    }
 
-console.log(tarefas)
-    //Marcar uma tarefa como concluída.
-    //Exibir a lista de tarefas.
-    //Contar quantas tarefas estão concluídas.
+const novaTarefa2 = {
+        id: 5,
+        descricao: "estudar PHP",
+        concluida: false,
+    };
+
+console.log(tarefas);
+
+//Adicionando novas tarefas
+tarefas.tarefa_4 = [novaTarefa1];
+tarefas.tarefa_5 = [novaTarefa2];
+
+console.log(tarefas);
+
+//Marcar uma tarefa como concluída.
+function Concluida(tarefas, id){
+    const chave = `tarefa_${id}`;
+    if (tarefas.hasOwnProperty(chave)){
+        delete tarefas[chave].concluida
+        tarefas[chave].conlcuida = true;
+        console.log(`Tarefa ${id} marcada como concluida`)
+    }
+    else{
+        console.log(`Tarefa ${id} não encontrada`)
+    }
+}
+
+Concluida(tarefas, 3)
+Concluida(tarefas, 1)
+Concluida(tarefas, 2)
+Concluida(tarefas, 4)
+Concluida(tarefas, 5)
+
+//Exibir a lista de tarefas.
+console.log(tarefas);
+
+//Contar quantas tarefas estão concluídas.
+function contarConcluidas(tarefas){
+    const tarefasArray = Object.values(tarefas);
+    return tarefasArray.filter(tarefa => tarefa.concluida).length;
+}
+
+
+const tarefasConcluidas = contarConcluidas(tarefas);
+console.log(`tarefas foram concluidas: ${tarefasConcluidas}`)
+        
