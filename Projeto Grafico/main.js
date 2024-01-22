873ed4fbad883cca88b402a7b7244c0ac2aebce6
Pablo Roberto <pablomoura2016@gmail.com>
2024-01-22 16:48:16 -0300

    coemçando a fazer o grafico

--- Projeto Grafico/main.js
+++ Projeto Grafico/main.js
@@ -1,3 +1,31 @@
-$(document).ready(() =>{
-    
-})
+/* $(document).ready(() =>{
+
+    async function obterDados(){
+        const resposta = await fetch('./expenses-chart-component-main/data.json');
+        const dados = await resposta.json();
+        return dados.dias;
+    }
+
+    async function grafico(){
+        const dados = await obterDados();
+
+        const labels = dados.map(item => item.day); 
+        const valores = dados.map(item => item.amount);
+
+        const ctx = $('#meuGrafico').getContext('2d');
+
+        const grafico = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Valores por dias',
+                    data: valores,
+                    backgroundcolor: 'black'
+                }]
+            }
+        })
+    }
+
+    grafico();
+}) */
